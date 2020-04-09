fcb50447eb970cd3b13c17a54f30b1f6ec7c9cb5
Mehdi <mehdi.zehri@epitech.eu>
2020-04-09 16:10:27 +0200

    edited views.py, we imported from flask render_template

--- app/views.py
+++ app/views.py
@@ -1,8 +1,8 @@
 from app import app
-from flask import jsonify
+from flask import Flask, render_template
 import pymysql as sql
 
 @app.route('/', methods=['GET'])
 @app.route('/index', methods=['GET'])
 def route_home():
-        return "Hello World\n"
+        return render_template("index.html")
