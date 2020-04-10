2a89442cfb353a55b62c39c3c2be5cd1002559e0
KinoFool <nicolas.mary@epitech.eu>
2020-04-10 12:53:42 +0200

    Verify if user is not already in base

--- app/register.py
+++ app/register.py
@@ -3,15 +3,33 @@ from flask import Flask, render_template, url_for, redirect, request
 import pymysql as sql
 from forms import SignUpForm
 from config import connection
+from string import lower
 
 def add_user():
-    username = request.form['username']
+    username = lower(request.form['username'])
     userpass = request.form['password']
+    base = user_in_base(username)
+    if base == False:
+        try:
+            with connection.cursor() as cursor:
+                sql = "INSERT INTO user (username, password) VALUES (%s, %s)"
+                cursor.execute(sql, (username, userpass))
+            connection.commit()
+            return render_template("index.html")
+        finally:
+            cursor.close()
+    else:
+        return render_template("index.html")
+
+def user_in_base(username):
     try:
         with connection.cursor() as cursor:
-            sql = "INSERT INTO user (username, password) VALUES (%s, %s)"
-            cursor.execute(sql, (username, userpass))
-        connection.commit()
-        return render_template("index.html")
+            sql = "SELECT COUNT(username) FROM user WHERE username =%s"
+            cursor.execute(sql, username)
+            result = cursor.fetchone()
+            if result[0] == 0:
+                return False
+            else:
+                return True
     finally:
-        connection.close()
+            cursor.close()
