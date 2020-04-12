3392141cba5e706670051e1492e415c39b90986a
Jose Jonathan <jonathan-emmanuel.jose@epitech.eu>
2020-04-12 17:50:04 +0200

    task are reliable

--- app/task.py
+++ app/task.py
@@ -3,11 +3,11 @@ from flask import Flask, render_template, url_for, redirect, request
 import pymysql as sql
 from config import connection
 
-def create_task(user_id, title, begin, end):
+def create_task(user_id, title, begin, end, status):
     try:
         with connection.cursor() as cursor:
-            sql = "INSERT INTO task (title, begin, end) VALUES (%s, %s, %s)"
-            cursor.execute(sql, (title, begin, end))
+            sql = "INSERT INTO task (title, begin, end, status) VALUES (%s, %s, %s, %s)"
+            cursor.execute(sql, (title, begin, end, status))
         connection.commit()
         with connection.cursor() as cursor:
             sql = "SELECT MAX(task_id) FROM task"
